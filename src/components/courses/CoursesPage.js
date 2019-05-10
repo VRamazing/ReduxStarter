28ea8da73c2f2a5582417c31c9aaeba974e9171f
vignesh ramesh <vignesh.ramesh@decorist.com>
2019-05-10 15:36:15 +0530

    Initial project with reducers and action creators

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+class CoursesPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      course: {
+        title: ''
+      }
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onCourseInputChange = this.onCourseInputChange.bind(this);
+  }
+  
+  onSubmit(e){
+    e.preventDefault();
+    this.props.addCourse(this.state.course)
+  }
+
+  onCourseInputChange(e){
+    this.setState({
+      course: {title: e.target.value}
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Courses</h2>
+        <form onSubmit={this.onSubmit}>
+            <input type="text" onChange={this.onCourseInputChange}/> 
+            <input type="submit" value="Add course" />
+        </form>
+        {this.props.courses.map((item) => <li key={item.title}>{item.title}</li>)}
+      </div>
+    )
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  addCourse: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCourse: (course) => {
+      dispatch(courseActions.createCourse(course))
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore({courses: []});
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  ,
+  document.getElementById("app")
+);
